1f88af8404d3b04979cbc57697c7c9bdd1d242df
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-12 04:11:03 -0500

    Add Validations and Unit Tests

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Like, type: :model do
+  subject { Like.new(author_id: 123, post_id: 70) }
+
+  before { subject.save }
+
+  it "post id should be present" do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "author id should be present" do
+    subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+end
