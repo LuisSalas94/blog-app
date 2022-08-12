b5fd307ff92c2d53130fefdf967727382a3166ca
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-12 01:33:48 -0500

    Add Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User'
 
+  validates :title, presence: true, length: {maximum: 250}  
+  validates :comments_counter, numericality:{only_integer: true, greater_than_or_equal_to: 0}
+  validates :likes_counter, numericality:{only_integer: true, greater_than_or_equal_to: 0}
+
   def self.update_post(user)
     user.increment!(:posts_counter)
   end
