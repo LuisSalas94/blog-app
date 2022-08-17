b47be4c7fa70a04d89736d25ed54e37c8f14ccd2
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-16 19:47:37 -0500

    Fix Linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,39 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/users/1/posts' }
 
-    before(:example) { get "/users/1/posts" }
-
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "renders index template" do
-      expect(response).to render_template("index")
+    it 'renders index template' do
+      expect(response).to render_template('index')
     end
 
-    it "body includes correct placeholder text" do
-      expect(response.body).to include("List of all posts for a given user")
+    it 'body includes correct placeholder text' do
+      expect(response.body).to include('List of all posts for a given user')
     end
-    
   end
 
-  describe "GET /show" do
-  before(:example) { get "/users/1/posts/1" }
+  describe 'GET /show' do
+    before(:example) { get '/users/1/posts/1' }
 
-  it "returns http success" do
-    expect(response).to have_http_status(:success)
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders show template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Details for a certain posts for a given user')
+    end
   end
-
-  it "renders show template" do
-    expect(response).to render_template("show")
-  end
-
-  it "response body includes correct placeholder text" do
-    expect(response.body).to include("Details for a certain posts for a given user")
-  end
-end
-
- 
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,35 +1,35 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    before(:example) { get "/users" }
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/users' }
 
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "renders index template" do
-      expect(response).to render_template("index")
+    it 'renders index template' do
+      expect(response).to render_template('index')
     end
 
-    it "response body includes correct placeholder text" do
-      expect(response.body).to include("List of users")
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('List of users')
     end
   end
 
-  describe "GET /show" do
-    before(:example) { get "/users/1" }
+  describe 'GET /show' do
+    before(:example) { get '/users/1' }
 
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "renders show template" do
-      expect(response).to render_template("show")
+    it 'renders show template' do
+      expect(response).to render_template('show')
     end
 
-    it "body includes correct placeholder text" do
-      expect(response.body).to include("Details for given user")
+    it 'body includes correct placeholder text' do
+      expect(response.body).to include('Details for given user')
     end
   end
 end
