36a4178bca2200f4486c0b1eedc2425abaedc8fb
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-16 19:27:42 -0500

    Add tests

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /index" do
+
+    before(:example) { get "/users/1/posts" }
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders index template" do
+      expect(response).to render_template("index")
+    end
+
+    it "body includes correct placeholder text" do
+      expect(response.body).to include("List of all posts for a given user")
+    end
+    
+  end
+
+  describe "GET /show" do
+  before(:example) { get "/users/1/posts/1" }
+
+  it "returns http success" do
+    expect(response).to have_http_status(:success)
+  end
+
+  it "renders show template" do
+    expect(response).to render_template("show")
+  end
+
+  it "response body includes correct placeholder text" do
+    expect(response.body).to include("Details for a certain posts for a given user")
+  end
+end
+
+ 
+end
