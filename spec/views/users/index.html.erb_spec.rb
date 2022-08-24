213cd6142f86f7786c2f87367a26509af658be12
mar <marilirulita@gmail.com>
2022-08-24 00:42:02 -0700

    Add test for user index show usename

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'users/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'User', type: :system do
+  describe 'index page' do
+    it 'shows the right content' do
+      first_user = User.create(name: "Bob", image: "https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg", bio: "Peruvian Chef.", posts_counter: 0)
+      visit root_path
+      expect(page).to have_content('Bob')
+    end
+  end
 end
