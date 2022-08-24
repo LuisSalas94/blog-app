95ce26a73f2b98ebcf50a8ad3cf03515263f8ea0
Fernando Salas <luisfernandosalasg@gmail.com>
2022-08-24 03:01:54 -0500

    Add index page test

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'User', type: :system do
   describe 'index page' do
+    before do
+      @first_user = User.create(name: "Bob", image: "https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg", bio: "Peruvian Chef.", posts_counter: 0)
+    end
+
     it 'shows the right content' do
-      first_user = User.create(name: "Bob", image: "https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg", bio: "Peruvian Chef.", posts_counter: 0)
       visit root_path
       expect(page).to have_content('Bob')
     end
+
+    it 'show profile picture' do
+      visit root_path
+      expect(page).to have_css("img[src*='https://icon-library.com/images/person-png-icon/person-png-icon-29.jpg']")
+    end
+
+    it 'see the number of posts' do
+      visit root_path
+      expect(page).to have_content('0')
+    end
+
+    it 'when click on user, redirect to user show page' do
+      visit root_path
+      click_on 'Bob'
+      expect(current_path).to eq(user_path(@first_user))
+    end
+
   end
 end
